2040a50e3b4ed7caad0dfc0ca7cf7685a3d65926
Joern Bernhardt <joern.bernhardt@compose.us>
2023-05-16 16:48:04 +0200

    feat: Add falling down and some falling state
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -3,39 +3,71 @@
 	import Skyline from './Skyline.svelte';
 
 	import { createWordWithRegex } from '$lib/regex-word-generator';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 
 	const [a, b, c] = [createWordWithRegex(), createWordWithRegex(), createWordWithRegex()];
-	let words = writable<{ position: { x: number; y: number }; word: string; isSolved: boolean }[]>([
-		{ word: a.word, position: { x: 25, y: 50 }, isSolved: false },
-		{ word: b.word, position: { x: 50, y: 25 }, isSolved: false },
-		{ word: c.word, position: { x: 75, y: 0 }, isSolved: false }
+	let words = writable<
+		{ position: { x: number; y: number }; word: string; state: 'solved' | 'crashed' | 'falling' }[]
+	>([
+		{ word: a.word, position: { x: 25, y: 0 }, state: 'falling' },
+		{ word: b.word, position: { x: 50, y: -25 }, state: 'falling' },
+		{ word: c.word, position: { x: 75, y: -50 }, state: 'falling' }
 	]);
 	let regexes = writable<[RegExp, RegExp, RegExp]>([a.regex, b.regex, c.regex]);
+	let arcadeScreen: HTMLElement;
 
 	function checkWords(regex: RegExp) {
 		let solvedWithRegex = 0;
 		for (let i = 0; i < $words.length; i++) {
 			const word = $words[i];
 			console.log('testing', regex, 'against', word.word);
-			if (word.isSolved) {
+			if (word.state !== 'falling') {
 				continue;
 			}
 			if (regex.test(word.word)) {
-				$words[i].isSolved = true;
+				$words[i].state = 'solved';
 				solvedWithRegex++;
 			}
 		}
 	}
+
+	function loop(maxHeightOfScreen: number) {
+		requestAnimationFrame(() => {
+			$words = $words.map((word) => {
+				console.log({ y: word.position.y, maxHeightOfScreen, state: word.state });
+				const y = Math.min(maxHeightOfScreen, word.position.y + 1);
+				return {
+					...word,
+					state: word.position.y === maxHeightOfScreen ? 'crashed' : word.state,
+					position: { ...word.position, y }
+				};
+			});
+			if ($words.some((w) => w.state !== 'crashed')) {
+				loop(maxHeightOfScreen);
+			}
+		});
+	}
+
+	onMount(() => {
+		if (browser) {
+			const maxHeightOfScreen = arcadeScreen.scrollHeight;
+			console.log({ maxHeightOfScreen });
+			loop(maxHeightOfScreen);
+		}
+	});
 </script>
 
 <div class="game">
-	<section class="falling-words">
+	<section class="falling-words" bind:this={arcadeScreen}>
 		<div class="words">
-			{#each $words as { word, position: { x, y }, isSolved }}
-				<div class="word" class:isSolved style="top: {y}%; left: {x}%">{word}</div>
+			{#each $words as { word, position: { x, y }, state }}
+				<div class="word" class:isSolved={state === 'solved'} style="top: {y}px; left: {x}%">
+					{word}
+				</div>
 			{/each}
 		</div>
-		<Skyline downPercentage={0.9} />
+		<Skyline downPercentage={$words.filter((w) => w.state === 'crashed').length / 3} />
 	</section>
 
 	<section class="buttons">
